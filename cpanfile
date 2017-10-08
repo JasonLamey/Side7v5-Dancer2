b367cb2d6600965b3f0abc4f2d185f2c29bf6df0
Jason Lamey <jasonlamey@gmail.com>
2017-10-08 02:01:13 -0400

    Setup of Dancer2 Plugins and Modules.

--- cpanfile
+++ cpanfile
@@ -1,4 +1,9 @@
-requires "Dancer2" => "0.203001";
+requires "Dancer2"                            => "0.203001";
+requires "Dancer2::Plugin::Auth::Extensible"  => '0.704';
+requires "Dancer2::Session::Cookie"           => '0.008';
+requires "Dancer2::Plugin::Flash"             => '0.03';
+requires "Dancer2::Plugin::DBIC"              => '0.0100';
+requires "Dancer2::Plugin::Ajax"              => '0.300000';
 
 recommends "YAML"             => "0";
 recommends "URL::Encode::XS"  => "0";
--- lib/Side7.pm
+++ lib/Side7.pm
@@ -1,10 +1,55 @@
 package Side7;
-use Dancer2;
 
-our $VERSION = '0.1';
+use strict;
+use warnings;
+
+# Dancer2 and Plugins/Modules
+use Dancer2;
+use Dancer2::Session::Cookie;
+use Dancer2::Plugin::Auth::Extensible;
+use Dancer2::Plugin::Flash;
+use Dancer2::Plugin::Ajax;
+use Dancer2::Plugin::DBIC;
+
+# Third-party modules
+
+our $VERSION = '5.0';
+
+
+=head1 NAME
+
+Side7
+
+
+=head1 DESCRIPTION
+
+Primary library and route handler for the Side 7 web app.
+
+
+=head1 ROUTES
+
+
+=head2 General Routes
+
+=cut
 
 get '/' => sub {
     template 'index';
 };
+
+
+=head2 Routes Requiring User Login
+
+=cut
+
+
+=head2 Admin Routes
+
+=cut
+
+
+=head1 ADDITIONAL METHODS
+
+=cut
 
 true;
