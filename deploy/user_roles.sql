b26abaa3d2a7de5c6cf73717c9174ad002575230
Jason Lamey <jasonlamey@gmail.com>
2017-10-20 18:45:48 -0400

    Updated user_roles to be cleaner. Created starter migration too.

--- deploy/user_roles.sql
+++ deploy/user_roles.sql
@@ -10,8 +10,8 @@ CREATE TABLE `user_roles` (
   `role_id` bigint(20) unsigned NOT NULL,
   PRIMARY KEY (`user_id`,`role_id`),
   KEY `role_id` (`role_id`),
-  CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+  CONSTRAINT `user_roles_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `user_roles_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE 
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 COMMIT;
---
+++ tools/migrate-4-5.pl
@@ -0,0 +1,236 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+$|++;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use DateTime;
+use DBI;
+use DBIx::Class;
+use Const::Fast;
+use Getopt::Long;
+Getopt::Long::Configure ( "bundling", "ignorecase_always" );
+use version; our $VERSION = version->declare("v2.0.0");
+use Term::ProgressBar;
+
+use Side7::Schema;
+
+const my $SCHEMA => Side7::Schema->db_connect();
+const my $DBH5   => $SCHEMA->storage->dbh;
+const my $DBH    => DBI->connect(
+                      "dbi:mysql:database=side7_v4;host=localhost", "webowner", "s7web",
+                      {
+                        RaiseError => 0,
+                        PrintError => 0
+                      }
+                    );
+const my $TODAY  => DateTime->today( time_zone => 'UTC' );
+
+
+=head1 NAME
+
+Side 7 Migration Script - v4 to v5
+
+
+=head1 DESCRIPTION
+
+This script pulls data from the v4 database, and reformats it to conform to the v5 database. It then inserts it into the new database.
+This script only moves items that are dynamic, and does not include lookup tables. Lookup tables are handled by sqitch deploys.
+
+
+=head2 Execution
+
+  ./migrate-4-5.pl [-v|--verbose] [-d|--dryrun] [-i|--interactive] [-h|--help]
+
+  -v|--verbose     => Verbose logging to the console
+  -d|--dryrun      => Run through the process, but do not commit to the new database
+  -i|--interactive => Confirm each table migration before processing it
+  -h|--help        => Display this help message
+
+=cut
+
+#########################################################
+# Main
+#########################################################
+
+our $verbose     = 0;
+our $dryrun      = 0;
+our $interactive = 0;
+our $help        = 0;
+
+GetOptions(
+  'verbose|v'     => \$verbose,
+  'dryrun|d'      => \$dryrun,
+  'interactive|i' => \$interactive,
+  'help|h'        => \$help,
+);
+
+if ( $help )
+{
+  show_help();
+  exit;
+}
+
+print "\nSide 7 DB Migration tool -- Version $VERSION\n";
+print "Verbose is ON\n"    if $verbose;
+print "Dry Run is ON\n"    if $dryrun;
+print "Interactive mode\n" if $interactive;
+print "\n";
+
+######################################
+
+migrate_users();
+
+######################################
+
+print "Shutting down.\n\n";
+$DBH->disconnect;
+
+
+#########################################################
+# Support subroutines
+#########################################################
+
+sub show_help
+{
+  print "\nSide 7 DB Migration tool -- Version $VERSION\n\n";
+  print "Migrates and normalizes v4 data to v5 format, and populates the v5 database.\n\n";
+  print "Usage:\n";
+  print "./$0 [-v|--verbose] [-d|--dryrun] [-i|--interactive] [-h|--help]\n\n";
+  print "\t-v|--verbose     => Verbose logging to the console\n";
+  print "\t-d|--dryrun      => Run through the process, but do not commit to the new database\n";
+  print "\t-i|--interactive => Confirm each table migration before processing it\n";
+  print "\t-h|--help        => Display this help message\n\n";
+}
+
+sub confirm_table
+{
+  my $table = shift;
+
+  printf "- Migrate table '%s' [Y/n]: ", $table;
+  my $confirm = <STDIN>;
+
+  chomp $confirm;
+
+  if ( $confirm eq '' || $confirm eq 'Y' )
+  {
+    printf "-- Migrating\n";
+    return 1;
+  }
+  else
+  {
+    printf "-- Skipping\n";
+    return 0;
+  }
+}
+
+sub truncate_table
+{
+  my $table = shift;
+  $DBH5->do( 'SET FOREIGN_KEY_CHECKS = 0;' );
+  $DBH5->do( sprintf( 'TRUNCATE TABLE %s;', $table ) );
+  $DBH5->do( 'SET FOREIGN_KEY_CHECKS = 1;' );
+  return;
+}
+
+sub migrate_users
+{
+  print "Users Table\n" if $verbose;
+  if ( $interactive )
+  {
+    if ( ! confirm_table( 'users' ) )
+    {
+      return;
+    }
+  }
+
+  # Cleanup the v5 tables from any previous migration attempts
+  print "- Truncating v5 table\n" if $verbose;
+  truncate_table( 'users' );
+
+  # v4 data pull
+  my $sth = $DBH->prepare(
+    'SELECT ua.*, uapi.biography, uapi.sex, uapi.birthdate, uapi.birthdate_mode, uapi.state, uapi.country
+       FROM user_accounts ua
+  LEFT JOIN user_account_personal_info uapi ON uapi.user_account_id = ua.id
+   ORDER BY id'
+  );
+  $sth->execute();
+  my $rv = $sth->fetchall_hashref( 'id' );
+
+  my $num_rows = scalar( keys ( %{$rv} ) );
+  printf "- v4 Rows found: %d\n", $num_rows if $verbose;
+
+  # Normalize Values
+  my %statuses = ( 'Pending' => 1, 'Active' => 2, 'Suspended' => 3, 'Disabled' => 4 );
+  my %genders  = ( 'Unspecified' => 1, 'Male' => 25, 'Female' => 16, 'Neither' => 28, 'Other' => 31 );
+  my %bday_vis = ( 'Full' => 'Public', 'No Year' => 'Hide Year', 'Hidden' => 'Private' );
+
+  # Import
+  print "- Importing rows\n" if $verbose;
+
+  # Set Up Progress Bar
+  my $progress = Term::ProgressBar->new(
+    {
+      name  => 'Users Table',
+      count => $num_rows,
+      ETA   => 'linear',
+    }
+  );
+  $progress->max_update_rate(1);
+  my $next_update = 0; my $i = 0;
+
+  foreach my $key ( sort keys ( %{$rv} ) )
+  {
+    my $row = $rv->{$key};
+
+    # Normalize birthdate
+    my $birthday = $TODAY->ymd;
+    if ( defined $row->{'birthdate'} )
+    {
+      my ( $year, $mon, $day ) = split( /-/, $row->{'birthdate'} );
+
+      if ( $year eq '0000' ) { $year = $TODAY->year; }
+      if ( $mon eq '00' )    { $mon  = $TODAY->mon; }
+      if ( $day eq '00' )    { $day  = $TODAY->day; }
+
+      $birthday = join( '-', $year, $mon, $day );
+    }
+
+    # Insert new record
+    if ( ! $dryrun )
+    {
+      my $new_user = $SCHEMA->resultset( 'User' )->create(
+        {
+          id                  => $row->{'id'},
+          username            => $row->{'username'},
+          password            => $row->{'password'},
+          email_address       => $row->{'email_address'},
+          first_name          => $row->{'first_name'},
+          last_name           => $row->{'last_name'},
+          birthday            => $birthday,
+          birthday_visibility => $bday_vis{$row->{'birthdate_mode'}},
+          user_status_id      => $statuses{$row->{'status'}},
+          gender_id           => $genders{$row->{'sex'}},
+          biography           => $row->{'biography'},
+          state               => $row->{'state'},
+          country_id          => 1, # Set to Undefined
+          confirmed           => ( ( uc($row->{'status'}) eq 'PENDING' ) ? 0 : 1 ),
+          confirm_code        => $row->{'email_confirmation_code'},
+          created_at          => $row->{'join_date'} . ' 00:00:00',
+        }
+      );
+    }
+
+    $next_update = $progress->update( $i ) if $i > $next_update;
+    $i++;
+  }
+  $progress->update( $num_rows ) if $num_rows >= $next_update;
+
+  $sth->finish;
+
+}
